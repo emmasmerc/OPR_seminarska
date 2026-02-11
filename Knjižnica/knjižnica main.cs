c57ed7feff97beffee1473f6c401e923ad89de2d
Emma Šmerc <emmasmerc.20655@dijak.sc-celje.si>
2026-02-11 22:54:32 +0100

    2. del OPR seminarske

--- Knjižnica/knjižnica main.cs
+++ Knjižnica/knjižnica main.cs
@@ -23,7 +23,35 @@ namespace Knjižnica
         }
     }
 
-    public class Knjiga : Gradivo
+    public interface IIzposodljivo
+    {
+        void Izposodi(Član član);
+        void Vrni();
+        bool JeIzposojeno {get;}
+    }
+
+    public abstract class IzposodljivoGradivo : Gradivo, IIzposodljivo
+    {
+        public bool JeIzposojeno {get; private set;}
+
+        protected IzposodljivoGradivo(string naslov, int leto)
+            : base(naslov, leto)
+        {
+            JeIzposojeno = false;
+        }
+
+        public virtual void Izposodi(Član član)
+        {
+            JeIzposojeno = true;
+        }
+
+        public virtual void Vrni()
+        {
+            JeIzposojeno = false;
+        }
+    }
+
+    public class Knjiga : IzposodljivoGradivo
     {
         public string Avtor {get; set;}
 
@@ -33,13 +61,19 @@ namespace Knjižnica
             Avtor = avtor;
         }
 
+        public override void Izposodi(Član član)
+        {
+            base.Izposodi(član);
+            Console.WriteLine($"Knjiga '{Naslov}' izposojena članu {član.Ime}");
+        }
+
         public override string ToString()
         {
             return $"{Naslov} – {Avtor} ({LetoIzdaje})";
         }
     }
 
-    public class DVD : Gradivo
+    public class DVD : IzposodljivoGradivo
     {
         public int Trajanje {get; set;}
 
@@ -47,6 +81,12 @@ namespace Knjižnica
             : base(naslov, letoIzdaje)
         {
             Trajanje = trajanje;
+        }
+
+        public override void Izposodi(Član član)
+        {
+            base.Izposodi(član);
+            Console.WriteLine($"DVD '{Naslov}' izposojen članu {član.Ime}");
         }
 
         public override string ToString()
@@ -73,11 +113,6 @@ namespace Knjižnica
             ČlanId = ++skupnoČlanov;
         }
 
-        public static int VrniŠteviloČlanov()
-        {
-            return skupnoČlanov;
-        }
-
         public override string ToString()
         {
             return $"{Ime} (ID: {ČlanId}, Zamudnina: {Zamudnina:0.00} €)";
@@ -101,123 +136,146 @@ namespace Knjižnica
         {
             return $"{Gradivo} – izposojeno: {Izposojevalec.Ime}";
         }
+    }
 
+    public class KnjižnicaSistem
+    {
+        private List<Gradivo> seznam = new List<Gradivo>();
+
+        public void Dodaj(Gradivo g)
+        {
+            seznam.Add(g);
+        }
+
+        public Gradivo this[int index]
+        {
+            get {return seznam[index];}
+            set {seznam[index] = value;}
+        }
+
+        public int Število => seznam.Count;
+    }
+
+    internal class knjižnica
+    {
+        static KnjižnicaSistem sistem = new KnjižnicaSistem();
         static List<Član> člani = new List<Član>();
-        static List<Gradivo> gradiva = new List<Gradivo>();
         static List<IzposojenoGradivo> izposoje = new List<IzposojenoGradivo>();
 
-        internal class knjižnica
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            while (true)
             {
-                while (true)
-                {
-                    Console.Clear();
-                    Console.WriteLine("KNJIŽNIČNI SISTEM");
-                    Console.WriteLine("1. Dodaj člana");
-                    Console.WriteLine("2. Dodaj knjigo");
-                    Console.WriteLine("3. Dodaj DVD");
-                    Console.WriteLine("4. Prikaži gradiva");
-                    Console.WriteLine("5. Prikaži člane");
-                    Console.WriteLine("6. Izposodi gradivo");
-                    Console.WriteLine("0. Izhod");
-                    Console.Write("\nIzbira: ");
+                Console.Clear();
+                Console.WriteLine("KNJIŽNIČNI SISTEM");
+                Console.WriteLine("1. Dodaj člana");
+                Console.WriteLine("2. Dodaj knjigo");
+                Console.WriteLine("3. Dodaj DVD");
+                Console.WriteLine("4. Prikaži gradiva");
+                Console.WriteLine("5. Prikaži člane");
+                Console.WriteLine("6. Izposodi gradivo");
+                Console.WriteLine("0. Izhod");
+                Console.Write("\nIzbira: ");
 
-                    switch (Console.ReadLine())
-                    {
-                        case "1": DodajČlana(); break;
-                        case "2": DodajKnjigo(); break;
-                        case "3": DodajDVD(); break;
-                        case "4": PrikažiGradiva(); break;
-                        case "5": PrikažiČlane(); break;
-                        case "6": IzposodiGradivo(); break;
-                        case "0": return;
-                        default:
-                            Console.WriteLine("Napačna izbira.");
-                            Console.ReadKey();
-                            break;
-                    }
+                switch (Console.ReadLine())
+                {
+                    case "1": DodajČlana(); break;
+                    case "2": DodajKnjigo(); break;
+                    case "3": DodajDVD(); break;
+                    case "4": PrikažiGradiva(); break;
+                    case "5": PrikažiČlane(); break;
+                    case "6": IzposodiGradivo(); break;
+                    case "0": return;
+                    default:
+                        Console.WriteLine("Napačna izbira.");
+                        Console.ReadKey();
+                        break;
                 }
             }
+        }
 
-            static void DodajČlana()
+        static void DodajČlana()
+        {
+            Console.Write("Ime člana: ");
+            string ime = Console.ReadLine();
+            člani.Add(new Član(ime));
+            Console.WriteLine("Član dodan.");
+            Console.ReadKey();
+        }
+
+        static void DodajKnjigo()
+        {
+            Console.Write("Naslov: ");
+            string naslov = Console.ReadLine();
+            Console.Write("Avtor: ");
+            string avtor = Console.ReadLine();
+            Console.Write("Leto izdaje: ");
+            int leto = int.Parse(Console.ReadLine());
+
+            sistem.Dodaj(new Knjiga(naslov, avtor, leto));
+            Console.WriteLine("Knjiga dodana.");
+            Console.ReadKey();
+        }
+
+        static void DodajDVD()
+        {
+            Console.Write("Naslov: ");
+            string naslov = Console.ReadLine();
+            Console.Write("Leto izdaje: ");
+            int leto = int.Parse(Console.ReadLine());
+            Console.Write("Trajanje (min): ");
+            int trajanje = int.Parse(Console.ReadLine());
+
+            sistem.Dodaj(new DVD(naslov, leto, trajanje));
+            Console.WriteLine("DVD dodan.");
+            Console.ReadKey();
+        }
+
+        static void PrikažiGradiva()
+        {
+            Console.WriteLine("\nGradiva:");
+            for (int i = 0; i < sistem.Število; i++)
             {
-                Console.Write("Ime člana: ");
-                string ime = Console.ReadLine();
-                člani.Add(new Član(ime));
-                Console.WriteLine("Član dodan.");
+                Console.WriteLine($"{i}: {sistem[i]}");
+            }
+            Console.ReadKey();
+        }
+
+        static void PrikažiČlane()
+        {
+            Console.WriteLine("\nČlani:");
+            for (int i = 0; i < člani.Count; i++)
+            {
+                Console.WriteLine($"{i}: {člani[i]}");
+            }
+            Console.ReadKey();
+        }
+
+        static void IzposodiGradivo()
+        {
+            if (člani.Count == 0 || sistem.Število == 0)
+            {
+                Console.WriteLine("Ni članov ali gradiv.");
                 Console.ReadKey();
+                return;
             }
 
-            static void DodajKnjigo()
-            {
-                Console.Write("Naslov: ");
-                string naslov = Console.ReadLine();
-                Console.Write("Avtor: ");
-                string avtor = Console.ReadLine();
-                Console.Write("Leto izdaje: ");
-                int leto = int.Parse(Console.ReadLine());
+            PrikažiČlane();
+            Console.Write("Izberi člana: ");
+            int c = int.Parse(Console.ReadLine());
 
-                gradiva.Add(new Knjiga(naslov, avtor, leto));
-                Console.WriteLine("Knjiga dodana.");
-                Console.ReadKey();
+            PrikažiGradiva();
+            Console.Write("Izberi gradivo: ");
+            int g = int.Parse(Console.ReadLine());
+
+            if (sistem[g] is IIzposodljivo izposodljivo)
+            {
+                izposodljivo.Izposodi(člani[c]);
+                izposoje.Add(new IzposojenoGradivo(sistem[g], člani[c]));
             }
 
-            static void DodajDVD()
-            {
-                Console.Write("Naslov: ");
-                string naslov = Console.ReadLine();
-                Console.Write("Leto izdaje: ");
-                int leto = int.Parse(Console.ReadLine());
-                Console.Write("Trajanje (min): ");
-                int trajanje = int.Parse(Console.ReadLine());
-
-                gradiva.Add(new DVD(naslov, leto, trajanje));
-                Console.WriteLine("DVD dodan.");
-                Console.ReadKey();
-            }
-
-            static void PrikažiGradiva()
-            {
-                Console.WriteLine("\nGradiva");
-                for (int i = 0; i < gradiva.Count; i++)
-                {
-                    Console.WriteLine($"{i}: {gradiva[i]}");
-                }
-                Console.ReadKey();
-            }
-
-            static void PrikažiČlane()
-            {
-                Console.WriteLine("\nČlani");
-                for (int i = 0; i < člani.Count; i++)
-                {
-                    Console.WriteLine($"{i}: {člani[i]}");
-                }
-                Console.ReadKey();
-            }
-
-            static void IzposodiGradivo()
-            {
-                if (člani.Count == 0 || gradiva.Count == 0)
-                {
-                    Console.WriteLine("Ni članov ali gradiv.");
-                    Console.ReadKey();
-                    return;
-                }
-
-                PrikažiČlane();
-                Console.Write("Izberi člana: ");
-                int c = int.Parse(Console.ReadLine());
-
-                PrikažiGradiva();
-                Console.Write("Izberi gradivo: ");
-                int g = int.Parse(Console.ReadLine());
-
-                izposoje.Add(new IzposojenoGradivo(gradiva[g], člani[c]));
-                Console.WriteLine("Gradivo izposojeno.");
-                Console.ReadKey();
-            }
+            Console.WriteLine("Gradivo izposojeno.");
+            Console.ReadKey();
         }
     }
 }
